a95f74aee5a01c58e48a2456c25c53b06e2768d2
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2016-11-13 11:22:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
 
-
+app.get('/', function(req, res) {
+    res.send('<h1>Hello world</h1>');
+});
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
 }
 
-//Create a server
-var server = http.createServer(handleRequest);
- 
-server.listen(server_port, server_ip_address, function () {
+http.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
