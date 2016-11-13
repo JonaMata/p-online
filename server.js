91415017dc2b90c73c344d3e29a0dae7fac33601
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2016-11-13 11:14:27 +0100

    configured server correctly

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var http = require('http');
+
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
