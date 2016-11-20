b481df4d936e703a29ecb26157a7a690ef1bab61
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2016-11-20 22:58:25 +0100

    Fixed listener

--- server.js
+++ server.js
@@ -12,24 +12,7 @@ function initIPAdress() {
 
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var io = require('socket.io').use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://p-online.juanto3.me');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-}).listen(port, initIPAdress());
+var io = require('socket.io').listen(port, initIPAdress());
 console.log('port: ' + port);
 io.on('connection', function(socket) {
   var room = "";
