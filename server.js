b03e0dacae386250c2b6f3a52333d113f992d156
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2016-12-22 23:45:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var port = process.env.PORT || 8080;
 
-app.use('/static', express.static(path.join(__dirname, 'web')))
+app.use('/', express.static(path.join(__dirname, 'web')))
 
 http.listen(port, function(){
   console.log('listening on *: ' + port);
