32b3007eafc87e03b7b54b0ac40de427238185be
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2016-12-22 23:37:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-  res.sendfile( __dirname + 'web/index.html');
+  res.sendfile( __dirname + '/web/index.html');
 });
 
 app.use('/static', express.static(path.join(__dirname, 'web')))
